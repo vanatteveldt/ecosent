5b72a3a1314abd7d8ab0b95dc68bddecc90af661
Wouter van Atteveldt <wouter@vanatteveldt.com>
2020-09-01 22:41:03 +0200

    Add comparison with NB and SVM hyperparams

---
+++ src/data-processing/19_ml_comparison.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python3
+#DESCRIPTION: Run SVM on the prepared sentences
+#DEPENDS: data/intermediate/sentences_ml.csv
+#CREATES: data/intermediate/svm_predictions.csv
+
+import logging
+import csv
+import numpy as np
+import pandas as pd
+import random
+from pathlib import Path
+
+from sklearn.model_selection import cross_validate
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, Normalizer
+from sklearn.svm import SVC
+from sklearn.feature_extraction.text import TfidfVectorizer
+import deeplib as lib
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s %(name)-12s %(levelname)-5s] %(message)s')
+
+random_seed = 1
+np.random.seed(random_seed)
+random.seed(random_seed)
+
+data_root = Path.cwd()/"data"
+data_file = data_root/"intermediate"/"sentences_ml.csv"
+output_file = data_root/"intermediate"/"svm_grid.csv"
+
+N_REPEAT = 10
+
+PARAM_GRID = dict(
+    kernel=['linear', 'poly', 'rbf'],
+    C = [.01, .1, 1, 10, 100],
+    gamma=['scale', 'auto', .001, .01, .1],
+    )
+
+
+logging.info("Reading data")
+d = pd.read_csv(data_file)
+d = d.loc[d['gold'] == 0]
+
+experiments = list(lib.iter_grid(PARAM_GRID))
+with output_file.open('w') as outf:
+    w = csv.writer(outf)
+    w.writerow(['rep', 'experiment', 'kernel', 'C', 'gamma', 'fold', 'f1_macro', 'f1_micro', 'accuracy'])
+    for rep in range(N_REPEAT):
+        for i, settings in enumerate(experiments):
+
+            logging.info(f"[{rep}/{N_REPEAT}][{i}/{len(experiments)}] Fitting model: {settings}")
+            pipe = Pipeline([("Vectorizer", TfidfVectorizer(min_df=10)),
+                             ("scaler", Normalizer()),
+                             ("SVC", SVC(C=settings['C'], gamma=settings['gamma'], kernel=settings['kernel']))])
+            # Shuffle data
+            d = d.sample(frac=1).reset_index(drop=True)
+
+            scores = cross_validate(pipe, d.lemmata.values, d.value.values, cv=5, scoring=['f1_macro', 'f1_micro', 'accuracy'])
+            for f, (f1_macro, f1_micro, accuracy) in enumerate(zip(scores['test_f1_macro'], scores['test_f1_micro'], scores['test_accuracy'] )):
+                w.writerow([rep, i, settings['kernel'], settings['C'], settings['gamma'], f, f1_macro, f1_micro, accuracy])
+                outf.flush()
+
---
+++ src/data-processing/22_nb.py
@@ -0,0 +1,55 @@
+#! /usr/bin/env python3
+#DESCRIPTION: Run SVM on the prepared sentences
+#DEPENDS: data/intermediate/sentences_ml.csv
+#CREATES: data/intermediate/svm_predictions.csv
+
+import logging
+import csv
+import numpy as np
+import pandas as pd
+import random
+from pathlib import Path
+
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, Normalizer
+from sklearn.svm import SVC
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import TfidfVectorizer
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s %(name)-12s %(levelname)-5s] %(message)s')
+
+random_seed = 1
+np.random.seed(random_seed)
+random.seed(random_seed)
+
+data_root = Path.cwd()/"data"
+data_file = data_root/"intermediate"/"sentences_ml.csv"
+output_file = data_root/"intermediate"/"nb_predictions.csv"
+
+pipe = Pipeline([("Vectorizer", TfidfVectorizer(min_df=10)),
+                 ("scaler", Normalizer()),
+                 ("NB", MultinomialNB())])
+
+logging.info("Reading data")
+h = pd.read_csv(data_file)
+
+train = h.loc[h['gold'] == 0]
+test = h.loc[h['gold'] == 1]
+
+with output_file.open('w') as outf:
+    w = csv.writer(outf)
+    w.writerow(['id', 'confidence', 'value'])
+    logging.info(f"Fitting model")
+    train = train.sample(frac=1).reset_index(drop=True)
+
+    pipe.fit(train.lemmata.values, train.value.values)
+    predictions = pipe.predict(test.lemmata.values)
+    scores = pipe.predict_proba(test.lemmata.values)
+    
+    acc = sum([a==b for (a,b) in zip(predictions, test.value.values)]) / len(predictions)
+    logging.info(f"Accuracy on test set: {acc:.4}")
+    
+    for id, pred, score in zip(test.id.values, predictions, scores):
+        score = sorted(score, reverse=True)
+        confidence = score[0] - score[1]
+        w.writerow([id, confidence, pred])
+
